aaf3ec9a26c6d5fae6d4163465d2c45f8675ea15
KamiNoKod0mo <149252909+KamiNoKod0mo@users.noreply.github.com>
2024-09-15 12:11:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+#const fs = require('fs');
+#fs.writeFileSync('test.txt', 'CTF-BR{malicious_flag}');
+
 const fs = require('fs');
-fs.writeFileSync('test.txt', 'CTF-BR{malicious_flag}');
+
+// Mensagem para depuração
+console.log('Executing script...');
+
+// Tenta criar o arquivo test.txt
+try {
+    fs.writeFileSync('/app/test.txt', 'Test successful');
+    console.log('File created successfully.');
+} catch (error) {
+    console.error('Error creating file:', error);
+}
